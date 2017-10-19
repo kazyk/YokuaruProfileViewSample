9630d5ebfd740f1a4ecd16dd391a23971891da7b
kazuyuki takahashi <kazrbb@gmail.com>
2017-10-19 21:43:55 +0900

    level6

--- test/TableViewController.swift
+++ test/TableViewController.swift
@@ -20,8 +20,7 @@ class TableViewController: UITableViewController {
         iconView.clipsToBounds = true
         iconView.layer.borderColor = UIColor.white.cgColor
         iconView.layer.cornerRadius = 3
-        iconView.layer.borderWidth = 2
-        
+
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(refresh(_:)), for: .primaryActionTriggered)
     }
@@ -46,10 +45,36 @@ class TableViewController: UITableViewController {
     }
 
     override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let bottomRight = profileView.convert(profileView.bounds, to: nil)
+        if profileView.window == nil {
+            return
+        }
+        let bottomRight = profileView.convert(profileView.bounds, to: view.window)
         let frameInWindow = CGRect(x: 0, y: 0, width: bottomRight.maxX, height: bottomRight.maxY)
-        backgroundImageViewContainer.frame = backgroundImageViewContainer.superview!.convert(frameInWindow, from: nil)
+        backgroundImageViewContainer.frame = backgroundImageViewContainer.superview!.convert(frameInWindow, from: view.window)
         backgroundImageView.frame = backgroundImageViewContainer.bounds
+
+        let navbar = navigationController!.navigationBar
+        let navbarFrameInWindow = navbar.convert(navbar.bounds, to: view.window)
+        let progress = ((navbarFrameInWindow.maxY - bottomRight.minY) / navbarFrameInWindow.height)
+
+        let alpha = max(0.0, min(1.0, progress))
+        let white = 1 - alpha
+        navbar.setBackgroundImage(minimumImage(color: UIColor.groupTableViewBackground.withAlphaComponent(alpha)), for: .default)
+        navbar.titleTextAttributes = [.foregroundColor: UIColor(white: white, alpha: 1)]
+
+        if alpha > 0.5 {
+            UIApplication.shared.statusBarStyle = .default
+        } else {
+            UIApplication.shared.statusBarStyle = .lightContent
+        }
     }
 }
 
+func minimumImage(color: UIColor) -> UIImage {
+    UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+    color.setFill()
+    UIBezierPath(rect: CGRect(x: 0, y: 0, width: 1, height: 1)).fill()
+    let image = UIGraphicsGetImageFromCurrentImageContext()!
+    UIGraphicsEndImageContext()
+    return image
+}
