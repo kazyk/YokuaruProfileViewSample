28db06f05b7146d46b41d942949110f9235959d5
kazuyuki takahashi <kazrbb@gmail.com>
2017-10-19 16:45:10 +0900

    hoge

---
+++ test/TableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewController.swift
+//  test
+//
+//  Created by kazuyuki takahashi on 2017/10/19.
+//  Copyright © 2017年 kazuyuki takahashi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    @IBOutlet weak var iconView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        iconView.clipsToBounds = true
+        iconView.layer.borderColor = UIColor.white.cgColor
+        iconView.layer.cornerRadius = 3
+        iconView.layer.borderWidth = 2
+    }
+
+}
